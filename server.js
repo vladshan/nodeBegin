6bfd8a2280728a9ca6b15e937b539652c3624b6b
vladshan <vladshan@msn.com>
2016-12-13 14:54:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+//get build-in http service
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type":
-  "text/plain"});
+//module 'start' body 
+function start() {
+  function onRequest (request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Privet World!");
   response.end();
-  }).listen(8888);
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
+ 
