f604e82d103658c82005e1f35cb0244dadf865bf
vladshan <vladshan@msn.com>
2016-12-13 13:00:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require("http");
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type":
   "text/plain"});
-  response.write("Hello World");
+  response.write("Privet World!");
   response.end();
   }).listen(8888);
